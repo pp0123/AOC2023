1b1dc9137f2add0884403d03e5ba76be2b5c9cdd
pp0123 <pp0123@gmail.com>
2023-12-25 20:41:44 -0500

    part1

--- Day5/part1.py
+++ Day5/part1.py
@@ -1,34 +1,37 @@
 import re
+from timedspinner import printspin
 seeds = []
 
-def convert_number(s, d, r):
-    source = int(s)
-    destination = int(d)
-    ranges = r
-    #print ('s:',source,'d:',destination,'r:',ranges)
+def print_map(s,m):
+    print ('\n\n\n',s,len(m[s]))
+    for i in range(len(m[s])):
+        for num_times in range(m[s][i][2]):
+            s_num = m[s][i][1] + num_times
+            d_num = m[s][i][0] + num_times
+            print ('seed:',s_num,':',d_num)
 
-    for ds, ss, l in ranges:
-        dest_start = int(ds)
-        source_start = int(ss)
-        length = int(l)
-        print ('ds:',dest_start,'ss:',source_start,'l:',length)
-        print ('ss:',source_start,' s:', source,' s_l', source_start + length)
-        if source_start <= source < (source_start + length):
-            print ("YES:",dest_start + (source - source_start))
-            return dest_start + (source - source_start)
-    print ("NO:",destination)
-    return destination  # If no mapping is found, return the original source
+def get_mapping_value(k,m,v):
+    for i in range(len(m[k])):
+            for num_times in range(m[k][i][2]):
+                s_num = m[k][i][1] + num_times
+                d_num = m[k][i][0] + num_times
+                printspin(0.10)
+                #print ('seed:',s_num,':',d_num)
+                if v == s_num:
+                    #print ('Found:',s_num)
+                    return d_num
+    return v
 
-def find_lowest_location(seeds, maps):
-    current_numbers = seeds
-    print (seeds,maps)
-    for map_name, ranges in maps.items():  
-        print (map_name) 
-        print('rng:',ranges)    
-        current_numbers = [convert_number(num, num, ranges) for num in current_numbers]
-        print ('cn:',current_numbers)
-    #return
-    return min(current_numbers)
+def get_mapping_value2(k,m,v):
+    d_num = v
+    for i in range(len(m[k])):
+            #print ('v:',v, m[k][i][0],m[k][i][0]+m[k][i][2],(v>= m[k][i][0]) and v < (m[k][i][0]+m[k][i][2]))
+            if (v > m[k][i][1]) and v <= (m[k][i][1]+m[k][i][2]):
+                #found it in the range, how toset d_num?
+                d_num = m[k][i][0] + (v - m[k][i][1])
+                #print('D:',d_num)
+                return d_num
+    return v
 
 maps = {}
 current_map = []
@@ -36,42 +39,48 @@ j=0
 with open("./input.txt", "r") as file:
     data = file.readlines()
     header = data[0].strip('\n')
-    s = header.split(':')[1].split()
-    for i in range(1, len(s)):
-                seeds.append(int(s[i]))
-    print (seeds)
-    nextline =data[2].strip('\n')
-    #print (seeds)
-    map_name = nextline.split()[0]
-    #print ('map:',map_name)    
-    for index in range(3, len(data)):
-        current_line=data[index].strip('\n')   
-        #print ('current_line:',current_line)  
+    s = strtointlist(header.split(':')[1].split())
+    seeds = s
+    print ('header:',header,'\ns:',seeds)
+    map_name = ''
+
+    for index in range(2, len(data)):
+        current_line = data[index].strip()
+        #print ("CL:",current_line)
         if (current_line.endswith('map:')):
-            maps[map_name] = current_map
-            j=1
-            current_map=[]
-            #print (maps[map_name])
+            map_name = current_line.split()[0]
+            #print ("first if", map_name)
             continue
         if re.match(current_line,'^\n$'):
-            index +=1
-            nextline =data[index].strip('\n')
-            map_name = nextline.split()[0]
-            #print('MAPNAME:', map_name)
-            j+=1
+            map_name = ''
+            current_map = []
+            continue
         else:
             current_value=data[index].strip('\n').split()
             current_map.append([int(i) for i in current_value])
-            #maps[map_name]=data[index].strip('\n').split(':')
+            maps[map_name]=current_map
+            #current_map = []
+            #print('MAPNAME:', map_name,"current_map:",current_map)
+        
+    print ('Got Maps:',maps)
+  
+    all_locations=[]
+
+    #print_map('seed-to-soil',maps)
+    for i in range(len(seeds)):
+        soil = get_mapping_value2('seed-to-soil',maps,seeds[i])
+        #print ('s:', seeds[i],soil)
+        fertilizer = get_mapping_value2('soil-to-fertilizer',maps,soil)
+        water = get_mapping_value2('fertilizer-to-water',maps,fertilizer)
+        light = get_mapping_value2('water-to-light',maps,water)
+        temp = get_mapping_value2('light-to-temperature',maps,light)
+        humid = get_mapping_value2('temperature-to-humidity',maps,temp)
+        location = get_mapping_value2('humidity-to-location',maps,humid)
+        all_locations.append(location)
+        #print ('seed:',seeds[i],'location:',location)
+        #print ('soil-to-fertilizer map:',seed_to_soil(seeds[i],maps),soil_to_fertilizer(seed_to_soil(seeds[i],maps),maps))
+    print (min(all_locations))
 
 
-    for key, values in maps.items():
-        for i in range(len(values)):
-            print(key,'[',i,']:', values[i])    
-    #print ('\nSEEDS:',seeds)
-
-
-    result = find_lowest_location(seeds, maps)
-    print("Lowest location number:", result)
-
+  
 
